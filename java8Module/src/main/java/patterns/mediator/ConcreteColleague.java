fc09f200861d0b95188df64de5ec5dc6e7922afc
Aleksandr Gromov <alexandr.gromov@magenta-technology.com>
2017-12-21 18:01:23 +0400

    new commit

---
+++ java8Module/src/main/java/patterns/mediator/ConcreteColleague.java
@@ -0,0 +1,12 @@
+package patterns.mediator;
+
+public class ConcreteColleague extends Colleague {
+    public ConcreteColleague(Mediator m) {
+        super(m);
+    }
+
+    @Override
+    public void receive(String message) {
+        System.out.println("Colleague Received: " + message);
+    }
+}
---
+++ java8Module/src/main/java/patterns/mediator/MobileColleague.java
@@ -0,0 +1,12 @@
+package patterns.mediator;
+
+public class MobileColleague extends Colleague {
+    public MobileColleague(Mediator m) {
+        super(m);
+    }
+
+    @Override
+    public void receive(String message) {
+        System.out.println("Mobile Received: " + message);
+    }
+}
